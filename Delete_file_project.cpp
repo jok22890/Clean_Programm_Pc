9fadacd183611b98e113ce94d5ffc47b52e8d5fb
Jok <120654014+jok22890@users.noreply.github.com>
2026-05-16 21:47:52 +0300

    Add files via upload

--- Delete_file_project.cpp
+++ Delete_file_project.cpp
@@ -26,48 +26,59 @@ private:
 
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	std::cout << "\t\tЭто программа создана для облегченного удаления файлов если их много!\n";
-	std::cout << "--------------------------------------------------------------------------------------------------------------------------\n";
-	std::cout << "Ввидите путь, который хотите очистить заменяя слеши на / ";
-	std::string path_;
-	std::cin >> path_;
-	fs::path path = path_;
-
-	std::cout << "Введи какой формат файла ты хочешь чтобы удалились\n";
-	std::string type;
-	std::cin >> type;
-
-	std::transform(type.begin(), type.end(), type.begin(), [](unsigned char c) {
-		return std::tolower(c);
-		});
-	Timer timer;
-	if (fs::exists(path))
+	try
 	{
-		std::cout << "Такая директория существует начинаю удаление\n\n";
-		std::vector<std::shared_ptr<fs::path>>vec;
-		
-		for (const auto& i : fs::directory_iterator(path))
+		SetConsoleCP(1251);
+		SetConsoleOutputCP(1251);
+		std::cout << "\t\tЭто программа создана для облегченного удаления файлов если их много!\n";
+		std::cout << "--------------------------------------------------------------------------------------------------------------------------\n";
+		std::cout << "Ввидите путь, который хотите очистить заменяя слеши на / ";
+		std::string path_;
+		std::getline(std::cin, path_);
+		fs::path path = path_;
+
+		std::cout << "Введи какой формат файла ты хочешь чтобы удалились\n";
+		std::string type;
+		std::cin >> type;
+
+		std::transform(type.begin(), type.end(), type.begin(), [](unsigned char c) {
+			return std::tolower(c);
+			});
+		Timer timer;
+		if (fs::exists(path.string()))
 		{
-			if (i.path().extension() == type)
+			std::cout << "Такая директория существует начинаю удаление\n\n";
+			std::vector<std::shared_ptr<fs::path>>vec;
+
+			for (const auto& i : fs::directory_iterator(path))
 			{
-				vec.push_back(std::make_shared<fs::path>(i.path()));
+				if (i.path().extension() == type)
+				{
+					vec.push_back(std::make_shared<fs::path>(i.path()));
+				}
 			}
+			std::cout << "Найдено " << vec.size() << " по вашему запросу\n";
+			for (const auto& i : vec)
+			{
+				std::string file = i->filename().string();
+				std::cout << file << " удалён" << '\n';
+				fs::remove(*i);
+			}
+			std::cout << "\nВсе файлы " << type << " удалены\n";
 		}
-		std::cout << "Найдено " << vec.size() << " по вашему запросу\n";
-		for (const auto& i : vec)
+		else
 		{
-			std::string file = i->filename().string();
-			std::cout << file << " удалён" << '\n';
-			fs::remove(*i);
+			std::cout << "Такой директории не существует\n";
 		}
+		
+		
 	}
-	else
+	catch (const std::exception&ex)
 	{
-		std::cout << "Такой директории не существует\n";
+		std::cout << ex.what() << '\n';
 	}
-	std::cout << "\nВсе файлы " << type << " удалены\n";
-
+	std::cin.ignore(10000, '\n');
+	std::cout << "Press Enter to exit...";
+	std::cin.get();
 	return 0;
 }
